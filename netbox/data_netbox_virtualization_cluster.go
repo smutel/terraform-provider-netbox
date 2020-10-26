71245f897db538ab9fcf46b5677079ee59ce36c5
smutel <samuel.mutel@gmail.com>
2020-10-26 21:17:03 +0100

    feat: Add Cluster data

---
+++ netbox/data_netbox_virtualization_cluster.go
@@ -0,0 +1,52 @@
+package netbox
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+	netboxclient "github.com/netbox-community/go-netbox/netbox/client"
+	"github.com/netbox-community/go-netbox/netbox/client/virtualization"
+)
+
+func dataNetboxVirtualizationCluster() *schema.Resource {
+	return &schema.Resource{
+		Read: dataNetboxVirtualizationClusterRead,
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validation.StringLenBetween(1, 100),
+			},
+		},
+	}
+}
+
+func dataNetboxVirtualizationClusterRead(d *schema.ResourceData,
+	m interface{}) error {
+	client := m.(*netboxclient.NetBoxAPI)
+
+	name := d.Get("name").(string)
+
+	resource := virtualization.NewVirtualizationClustersListParams().WithName(
+		&name)
+
+	list, err := client.Virtualization.VirtualizationClustersList(resource, nil)
+	if err != nil {
+		return err
+	}
+
+	if *list.Payload.Count < 1 {
+		return fmt.Errorf("Your query returned no results. " +
+			"Please change your search criteria and try again.")
+	} else if *list.Payload.Count > 1 {
+		return fmt.Errorf("Your query returned more than one result. " +
+			"Please try a more specific search criteria.")
+	}
+
+	d.SetId(strconv.FormatInt(list.Payload.Results[0].ID, 10))
+
+	return nil
+}
