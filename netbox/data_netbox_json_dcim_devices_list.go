af40682efa6a14ad5091be56fd22614116eab2b4
Oleg Neychev <oleg.neychev@namecheap.com>
2021-08-31 14:04:29 +0300

    feat: add filters and limit params to devices_list

--- netbox/data_netbox_json_dcim_devices_list.go
+++ netbox/data_netbox_json_dcim_devices_list.go
@@ -1,37 +1,85 @@
 package netbox
 
 import (
-        "encoding/json"
+	"encoding/json"
+	"fmt"
 
-        "github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-        netboxclient "github.com/netbox-community/go-netbox/netbox/client"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	netboxclient "github.com/netbox-community/go-netbox/netbox/client"
+	"github.com/netbox-community/go-netbox/netbox/client/dcim"
 )
 
 func dataNetboxJSONDcimDevicesList() *schema.Resource {
-        return &schema.Resource{
-                Read: dataNetboxJSONDcimDevicesListRead,
+	return &schema.Resource{
+		Read: dataNetboxJSONDcimDevicesListRead,
 
-                Schema: map[string]*schema.Schema{
-                        "json": {
-                                Type:     schema.TypeString,
-                                Computed: true,
-                        },
-                },
-        }
+		Schema: map[string]*schema.Schema{
+			"filter": {
+				Type:     schema.TypeSet,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"value": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+					},
+				},
+			},
+			"limit": {
+				Type:     schema.TypeInt,
+				Optional: true,
+				Default:  0,
+			},
+			"json": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
 }
 
 func dataNetboxJSONDcimDevicesListRead(d *schema.ResourceData, m interface{}) error {
-        client := m.(*netboxclient.NetBoxAPI)
+	client := m.(*netboxclient.NetBoxAPI)
 
-        list, err := client.Dcim.DcimDevicesList(nil, nil)
-        if err != nil {
-                return err
-        }
+	params := dcim.NewDcimDevicesListParams()
+	limit := int64(d.Get("limit").(int))
+	if filter, ok := d.GetOk("filter"); ok {
+		var filterParams = filter.(*schema.Set)
+		for _, f := range filterParams.List() {
+			k := f.(map[string]interface{})["name"]
+			v := f.(map[string]interface{})["value"]
+			vString := v.(string)
+			switch k {
+			case "name":
+				params.Name = &vString
+			case "name_ic":
+				params.NameIc = &vString
+			case "tag":
+				params.Tag = &vString
+			case "cluster_id":
+				params.ClusterID = &vString
+			default:
+				return fmt.Errorf("'%s' is not a supported filter parameter", k)
+			}
+		}
+	} else {
+		params.Limit = &limit
+	}
 
-        j, _ := json.Marshal(list.Payload.Results)
+	list, err := client.Dcim.DcimDevicesList(params, nil)
+	if err != nil {
+		return err
+	}
 
-        d.Set("json", string(j))
-        d.SetId("NetboxJSONDcimDevicesList")
+	j, _ := json.Marshal(list.Payload.Results)
 
-        return nil
+	d.Set("json", string(j))
+	d.SetId("NetboxJSONDcimDevicesList")
+
+	return nil
 }
