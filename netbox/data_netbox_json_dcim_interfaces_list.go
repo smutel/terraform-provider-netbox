5f1c29b4de76f4556af6af88f3dbe40a0d642c8a
Oleg Neychev <oleg.neychev@namecheap.com>
2021-08-31 14:20:09 +0300

    feat: add devicename and limit params

--- netbox/data_netbox_json_dcim_interfaces_list.go
+++ netbox/data_netbox_json_dcim_interfaces_list.go
@@ -1,37 +1,55 @@
 package netbox
 
 import (
-        "encoding/json"
+	"encoding/json"
 
-        "github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-        netboxclient "github.com/netbox-community/go-netbox/netbox/client"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	netboxclient "github.com/netbox-community/go-netbox/netbox/client"
+	"github.com/netbox-community/go-netbox/netbox/client/dcim"
 )
 
 func dataNetboxJSONDcimInterfacesList() *schema.Resource {
-        return &schema.Resource{
-                Read: dataNetboxJSONDcimInterfacesListRead,
+	return &schema.Resource{
+		Read: dataNetboxJSONDcimInterfacesListRead,
 
-                Schema: map[string]*schema.Schema{
-                        "json": {
-                                Type:     schema.TypeString,
-                                Computed: true,
-                        },
-                },
-        }
+		Schema: map[string]*schema.Schema{
+			"devicename": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"limit": {
+				Type:     schema.TypeInt,
+				Optional: true,
+				Default:  0,
+			},
+			"json": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
 }
 
 func dataNetboxJSONDcimInterfacesListRead(d *schema.ResourceData, m interface{}) error {
-        client := m.(*netboxclient.NetBoxAPI)
+	client := m.(*netboxclient.NetBoxAPI)
 
-        list, err := client.Dcim.DcimInterfacesList(nil, nil)
-        if err != nil {
-                return err
-        }
+	deviceName := d.Get("devicename").(string)
+	limit := int64(d.Get("limit").(int))
+	params := dcim.NewDcimInterfacesListParams()
+	if deviceName != "" {
+		params.Device = &deviceName
+	}
+	params.Limit = &limit
 
-        j, _ := json.Marshal(list.Payload.Results)
+	list, err := client.Dcim.DcimInterfacesList(params, nil)
+	if err != nil {
+		return err
+	}
 
-        d.Set("json", string(j))
-        d.SetId("NetboxJSONDcimInterfacesList")
+	j, _ := json.Marshal(list.Payload.Results)
 
-        return nil
+	d.Set("json", string(j))
+	d.SetId("NetboxJSONDcimInterfacesList")
+
+	return nil
 }
