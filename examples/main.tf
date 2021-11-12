e8086bc0d347eca4d8aba3809b74f64735cf70eb
smutel <12967891+smutel@users.noreply.github.com>
2021-11-12 17:29:43 +0100

    enh: Manage custom fields in prefix resource

--- examples/main.tf
+++ examples/main.tf
@@ -155,6 +155,48 @@ resource "netbox_ipam_prefix" "prefix_test" {
     name = "tag2"
     slug = "tag2"
   }
+
+  custom_field {
+    name = "cf_boolean"
+    type = "boolean"
+    value = "true"
+  }
+
+  custom_field {
+    name = "cf_date"
+    type = "date"
+    value = "2020-12-25"
+  }
+
+  custom_field {
+    name = "cf_text"
+    type = "text"
+    value = "some text"
+  }
+
+  custom_field {
+    name = "cf_integer"
+    type = "integer"
+    value = "10"
+  }
+
+  custom_field {
+    name = "cf_selection"
+    type = "selection"
+    value = "1"
+  }
+
+  custom_field {
+    name = "cf_url"
+    type = "url"
+    value = "https://github.com"
+  }
+
+  custom_field {
+    name = "cf_multiple_selection"
+    type = "multiple"
+    value = "0,1"
+  }
 }
 
 resource "netbox_ipam_ip_addresses" "ip_test" {
