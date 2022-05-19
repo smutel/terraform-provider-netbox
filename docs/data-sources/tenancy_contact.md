43d308959e63ae32aa927ccfbe70e48961ce1d25
Samuel Mutel <12967891+smutel@users.noreply.github.com>
2022-05-19 15:59:08 +0200

    feat: Add contact resource

---
+++ docs/data-sources/tenancy_contact.md
@@ -0,0 +1,21 @@
+# netbox\_tenancy\_contact Data Source
+
+Get info about tenancy contact from netbox.
+
+## Example Usage
+
+```hcl
+data "netbox_tenancy_contact" "contact_test" {
+  name = "John Doe"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+* ``name`` - (Required) The name of the contact.
+
+## Attributes Reference
+
+In addition to the above arguments, the following attributes are exported:
+* ``id`` - The id (ref in Netbox) of this object.
---
+++ netbox/data_netbox_tenancy_contact.go
@@ -0,0 +1,50 @@
+package netbox
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	netboxclient "github.com/smutel/go-netbox/netbox/client"
+	"github.com/smutel/go-netbox/netbox/client/tenancy"
+)
+
+func dataNetboxTenancyContact() *schema.Resource {
+	return &schema.Resource{
+		Read: dataNetboxTenancyContactRead,
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validation.StringLenBetween(1, 100),
+			},
+		},
+	}
+}
+
+func dataNetboxTenancyContactRead(d *schema.ResourceData, m interface{}) error {
+	client := m.(*netboxclient.NetBoxAPI)
+
+	name := d.Get("name").(string)
+
+	p := tenancy.NewTenancyContactsListParams().WithName(&name)
+
+	list, err := client.Tenancy.TenancyContactsList(p, nil)
+	if err != nil {
+		return err
+	}
+
+	if *list.Payload.Count < 1 {
+		return fmt.Errorf("Your query returned no results. " +
+			"Please change your search criteria and try again.")
+	} else if *list.Payload.Count > 1 {
+		return fmt.Errorf("Your query returned more than one result. " +
+			"Please try a more specific search criteria.")
+	}
+
+	d.SetId(strconv.FormatInt(list.Payload.Results[0].ID, 10))
+
+	return nil
+}
